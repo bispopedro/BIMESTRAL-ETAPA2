90b0d29b950a318c604a7fde096dc05abdebc110
bispopedroiago <pedroiagolimalimalima0203@gmail.com>
2025-08-15 07:57:42 -0300

    Update questão4.h

--- questão4.h
+++ questão4.h
@@ -7,15 +7,15 @@ int main() {
     scanf("%d", &velo);
 
     if (velo > 75) {
-        printf("[Reduza a velocidade imediatamente!!!]\n");
+        printf("[ Reduza a velocidade imediatamente !!! ]\n");
     } else if (velo > 65) {
-        printf("[Você está muito rápido emm, cuidado!]\n");
+        printf("[Você está muito rápido emm, cuidado ! ]\n");
     } else if (velo > 55) {
-        printf("[Tenha atenção! Você está acima da velocidade média.]\n");
+        printf("[ Tenha atenção! Você está acima da velocidade média. ]\n");
     } else if (velo > 45) {
-        printf("[Velocidade boa, continue assim.]\n");
+        printf("[ Velocidade boa, continue assim. ]\n");
     } else {
-        printf("[Você está dirigindo com calma. continuie...]\n");
+        printf("[ Você está dirigindo com calma. continuie... ]\n");
     }
 
     return 0;
