c7e7d1f5eaea9e07715a56300d38e06b929d8bee
bispopedroiago <pedroiagolimalimalima0203@gmail.com>
2025-08-14 22:40:28 -0300

    Atualizar o questao2.h

--- questao.h
+++ questao.h
@@ -1,17 +1,31 @@
+
 #include <stdio.h>
 
 int main() {
     int N;
     int num = 1; 
-    
-    printf("Digite um número inteiro positivo: ");
-    scanf("%d", &N);
 
-    //Triângulo
-    for (int i = 1; i <= N; i++) { //linhas
-        for (int j = 1; j <= i; j++) { //números por linha
+    printf("Digite um numero inteiro positivo: ");
+    scanf("%d", &N);
+    
+    if (N <= 0) {
+        printf("O numero deve ser positivo.\n");
+        return 1;
+    }
+
+    printf("\nTriangulo ate o numero %d:\n", N);
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= i; j++) {
+            
+            if (num > N) {
+                break; 
+            }
             printf("%d ", num);
             num++; 
+        }
+        if (num > N) {
+            break; 
         }
         printf("\n");
     }
